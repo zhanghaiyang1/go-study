5abb1f03ef89823c49f0ad5170265fdcddc9c4d5
ocean <z_haiyang@163.com>
2022-06-23 13:34:40 +0800

    feat：channel

---
+++ channel/exit/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/*
+无缓冲 channel 作为主子 goroutine 之间的信号传递的桥梁。通过信号传递，主 goroutine 在子 goroutine 运行结束之后再退出。
+*/
+func main(){
+	block := make(chan struct{})
+
+	go func ()  {
+		time.Sleep(time.Second)
+		// block <- struct{}{}
+		close(block)
+	}()
+	<- block
+	fmt.Println("Done")
+}
+
---
+++ channel/transmit_signal/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+/*
+无缓冲 channel 可用于两个 goroutine 之间传递信号
+*/
+func main(){
+	block := make(chan struct{})
+
+	go odd(block)
+	go even(block)
+	time.Sleep(time.Second)
+	fmt.Println("Done")
+}
+func odd(block chan struct{}){
+	for i := 1; i <= 100; i++ {
+		<- block
+		if i & 1 == 1 {
+			fmt.Println(i)
+		}
+	}
+}
+func even(block chan struct{}){
+	for i := 1; i <= 100; i++ {
+		block <- struct{}{}
+		if i & 1 == 0 {
+			fmt.Println(i)
+		}
+	}	
+}
